31ae7dc57bb2b4e3f2db0dce3c099c6460db40c1
Iván Parra <ivandarioparraiglesias@gmail.com>
2025-06-06 19:41:14 +0200

    Manejo de cadenas

---
+++ Python-Avanzado/manejo_cadenas.py
@@ -0,0 +1,18 @@
+# Dividir listas con split() (parsing)
+cadena = 'Hola Mundo'
+palabras = cadena.split()
+print(palabras)
+
+# Buscar con find()
+posicion = cadena.find('Mundo')
+print(f'Posición de la cadena Mundo: {posicion}')
+
+# Reemplazar con replace()
+nueva_cadena = cadena.replace('Mundo', 'GitHub')
+print(f'Nueva cadena reemplazada: {nueva_cadena}')
+
+# Limpiar una cadena con strip()
+cadena = '   Hola Mundo   '
+cadena_limpia = cadena.strip()
+print(f'Cadena limpia: {cadena_limpia}')
+

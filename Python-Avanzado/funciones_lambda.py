6932bbb8f7715d8a2421d6c7e7ec6450b07c5c1e
Iván Parra <ivandarioparraiglesias@gmail.com>
2025-06-09 19:55:58 +0200

    Uso de map y filter con lambda

--- Python-Avanzado/funciones_lambda.py
+++ Python-Avanzado/funciones_lambda.py
@@ -10,3 +10,16 @@ print(f'El cuadrado de 10: {cuadrado(10)}')
 cuadrado_lambda = lambda x : x ** 2
 
 print(f'El cuadrado de 3: {cuadrado_lambda(3)}')
+
+# Con map y lambda
+numeros = [1, 2, 3, 4, 5]
+
+# Aplicamos la función lambda para obtener el cuadrado de cada número
+cuadrados = list(map(lambda x: x ** 2, numeros))
+
+print(f'Resultado de usar map y lambda: {cuadrados}')
+
+# Con filter y lambda
+pares = list(map(lambda x: x % 2 == 0, numeros))
+
+print(f'Resultado de usar filter para números pares: {pares}')

7b3b1755e585b323b20a8c077eb5a7447ec73b12
Iván Parra <ivandarioparraiglesias@gmail.com>
2025-06-09 23:24:20 +0200

    Lanzar excepciones y else

--- Python-Avanzado/excepciones.py
+++ Python-Avanzado/excepciones.py
@@ -2,10 +2,16 @@ print('*** Excepciones en Python ***')
 
 def dividir(numerador, denominador):
     try:
+        # Revisamos si el denominador es igual a 0
+        if denominador == 0:
+            raise Exception('El denominador es igual a 0')
+        
         resultado = numerador / denominador
         print(f'Resultado de la división: {resultado}')
     except Exception as e:
         print(f'Error: {e}')
+    else:
+        print('No ocurrió ningún error')
     finally:
         print('Se termina de procesar la excepción')
     """ except ZeroDivisionError:

5894e3f54abf42bddb1ba78a1e532fd924d7f55a
Iván Parra <ivandarioparraiglesias@gmail.com>
2025-06-11 23:59:26 +0200

    Se añaden las funciones obtener_snacks, agregar_snacs, mostrar_snacks y get_snacks

--- Manejo-Archivos/maquina_snacks/servicio_snacks.py
+++ Manejo-Archivos/maquina_snacks/servicio_snacks.py
@@ -33,4 +33,28 @@ class ServicioSnack:
             print(f'Error al guardar los snacks en el archivo: {e}')
     
     def obtener_snack(self):
-        pass
+        snacks = []
+        
+        try:
+            with open(self.NOMBRE_ARCHIVO, 'r') as archivo:
+                
+                for linea in archivo:
+                    id_snack, nombre, precio = linea.strip().split(',')
+                    snack = Snack(nombre, float(precio))
+                    snacks.append(snack)
+                
+        except Exception as e:
+            print(f'Error al obtener la lista de snacks: {e}')
+            
+    def agregar_snacks(self, snack):
+        self.__snacks.append(snack)
+        self.guardar_snacks([snack])
+        
+    def mostrar_snacks(self):
+        print('--- Snacks en el inventario ---')
+        
+        for snack in self.__snacks:
+            print(snack.__str__())
+    
+    def get_snacks(self):
+        return self.__snacks

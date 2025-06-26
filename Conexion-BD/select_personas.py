f6166bc9a8bb590bcd873a07bb93454b2a861668
Iván Parra <ivandarioparraiglesias@gmail.com>
2025-06-26 20:28:02 +0200

    Vemos como se podría hacer un SELECT usando python y mariadb

---
+++ Conexion-BD/select_personas.py
@@ -0,0 +1,17 @@
+import mariadb
+
+# Creamos la conexión
+personas_db = mariadb.connect(
+    host='localhost',
+    port=3306,
+    user='root',
+    password='root',
+    database='personas_db'
+)
+
+cursor = personas_db.cursor()
+cursor.execute('SELECT * FROM personas')
+resultado = cursor.fetchall()
+
+for persona in resultado:
+    print(persona)

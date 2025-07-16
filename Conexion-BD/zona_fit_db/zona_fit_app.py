30aa6c7ea9b08df9ab33cb1cc60779c0a1026676
Iván Parra <ivandarioparraiglesias@gmail.com>
2025-07-16 10:34:43 +0200

    Creamos la clase AppZonaFit, encargada de manejar el menú con las opciones del cliente, y la función de listar_clientes()

---
+++ Conexion-BD/zona_fit_db/zona_fit_app.py
@@ -0,0 +1,58 @@
+from zona_fit_db.cliente import Cliente
+from zona_fit_db.cliente_dao import ClienteDAO
+
+class AppZonaFit:
+    
+    def __init__(self):
+        self.__cliente_dao = ClienteDAO()
+    
+    def get_clienteDAO(self):
+        return self.__cliente_dao
+    
+    def servicios_clientes(self):
+        salir = False
+        
+        while not salir:
+            try:
+                opcion = self.mostrar_menu()
+                salir = self.ejecutar_opcion(opcion)
+            except Exception as e:
+                print(f'Error: {e}')
+                
+    def mostrar_menu(self):
+        print(f'''Menu:
+        1. Listar clientes
+        2. Agregar cliente
+        3. Modificar cliente
+        4. Eliminar cliente
+        5. Salir''')
+        return int(input('Elige una opción: '))
+    
+    def ejecutar_opcion(self, opcion):
+        try:
+            if opcion == 1:
+                self.listar_clientes()
+            
+            elif opcion == 5:
+                return True
+            else:
+                print('Opción invalida, selecciona una opción del 1 - 5')
+        except ValueError:
+            print('Solo se aceptan valores numericos')
+        except Exception as e:
+            print(f'Error: {e}')
+            
+    def listar_clientes(self):
+        try:
+            clientes = self.get_clienteDAO().seleccionar()
+            print('\n*** Listado de clientes ***')
+            for cliente in clientes:
+                print(cliente)
+            
+        except Exception as e:
+            print(f'Error al listar clientes: {e}')
+
+# Programa principal
+if __name__ == '__main__':
+    zona_fit_app = AppZonaFit()
+    zona_fit_app.servicios_clientes()

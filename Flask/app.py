3d9b3966821d52d842c85babdc045bfc7e77db4c
Iván Parra <ivandarioparraiglesias@gmail.com>
2025-07-17 11:16:55 +0200

    Probamos a definir más rutas con el mismo contenido

--- Flask/app.py
+++ Flask/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/') #url: http://localhost:5000/
+@app.route('/index.html') #url: http://localhost:5000/index.html
 
 def inicio():
     app.logger.debug('Entramos al path de inicio /')
